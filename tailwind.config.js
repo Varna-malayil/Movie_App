f7e2ed9ec01d64a9ea72a4e4209021a2727593c1
Varna <varnamalayil123@gmail.com>
2025-03-29 17:35:21 +0530

    feature signup Working

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'color-2': '#f3f4f6', // Example color
+          'color-3': '#e5e7eb',
+          'color-4': '#333',
+        },
+      },
+    },
+  }
+  
